8d1fc29321133e0ee71f8e9a693db0e7034726a5
Alex Huang <hwangzhiming@gmail.com>
2019-12-03 20:48:58 +0800

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tx-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'graph-token-hero';
+}
